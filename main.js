2e6849a472215a4f1389a462e7cfb38a8533d36b
Prathyusha V <prathyusha28sep@gmail.com>
2026-02-04 20:23:40 +0530

    updated navbar links, mail links and skills section layout

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+document.querySelectorAll(".circle").forEach(circle => {
+    const percent =
+    circle.getAttribute("data-percent");
+    let current = 0;
+
+    const interval = setInterval(() => {
+        if (current >= percent) {
+            clearInterval(interval);
+        }
+        else {
+            current++;
+            circle.Style.background = `conic-gradient(
+                #00f7ff ${current * 3.6}deg,
+                #0d2a3a ${current * 3.6}deg
+            )`;
+circle.querySelector("span").innerText = current + "%";
+        }  
+    }, 20);
+});
+
 const form =
 document.getElementById("contactForm");
 const sendBtn =
